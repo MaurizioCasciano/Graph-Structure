727e68ea648ce95e9aa6db5f112f2bd44c096df9
Maurizio Casciano <mauriziocasciano7@gmail.com>
2022-12-06 15:58:05 +0100

    Spring Initializer
    https://start.spring.io/#!type=maven-project&language=java&platformVersion=3.0.1-SNAPSHOT&packaging=jar&jvmVersion=19&groupId=dev.casciano&artifactId=graph-structure&name=Graph-Structure&description=Graph%20Structure&packageName=dev.casciano.graph-structure&dependencies=web,data-neo4j

---
+++ src/main/java/dev/casciano/graphstructure/GraphStructureApplication.java
@@ -0,0 +1,13 @@
+package dev.casciano.graphstructure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraphStructureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GraphStructureApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/casciano/graphstructure/GraphStructureApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.casciano.graphstructure;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraphStructureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
